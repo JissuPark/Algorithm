11a035fa6db1e599ab92bcc73d8d69368b08f2c2
JissuPark <qkrwltn9412@gmail.com>
2021-01-06 00:32:52 +0900

    [ADD] BOJ - 10951 by java

---
+++ BOJ/ApulsB4.java
@@ -0,0 +1,14 @@
+package com.company.BOJ;
+
+import java.util.Scanner;
+
+public class ApulsB4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println(A + B);
+        }
+    }
+}
