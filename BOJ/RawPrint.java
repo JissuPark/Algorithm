a17b911351d8b583f3fbff5f7a5418758186aa48
JissuPark <qkrwltn9412@gmail.com>
2021-01-09 02:48:26 +0900

    [ADD] BOJ - 11719, 11720 by java

---
+++ BOJ/RawPrint.java
@@ -0,0 +1,12 @@
+package com.company.BOJ;
+
+import java.util.Scanner;
+
+public class RawPrint {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
