d9a79572a4edb41cb6869f2f40894b03d4155e77
JissuPark <qkrwltn9412@gmail.com>
2021-02-08 23:51:29 +0900

    [ADD] BOJ - 1463 by python

--- BOJ/1463.py
+++ BOJ/1463.py
@@ -1,11 +1,16 @@
-from sys import stdin
+from sys import stdin, maxsize
 
 input = stdin.readline
 
 
 def making_1(n):
-
-    return
+    DP = [maxsize for _ in range(n)]
+    DP.append(0)
+    for i in range(n, -1, -1):
+        if i*3 <= n: DP[i] = min(DP[i], DP[i*3]+1)
+        if i*2 <= n: DP[i] = min(DP[i], DP[i*2]+1)
+        if i+1 <= n: DP[i] = min(DP[i], DP[i+1]+1)
+    return DP[1]
 
 
 if __name__ == "__main__":
